134c14d591e26936c298a789821c810e186b8d43
AAAlex <147448054+AAAlex384@users.noreply.github.com>
2026-02-10 18:31:05 +0000

    Version number updated

--- scripts/logos.js
+++ scripts/logos.js
@@ -6,7 +6,14 @@ function updateLogo(logoSrc) {
   
 }
 
+function updateVer(versionNum) {
+  const version = document.querySelector('#version');
+  version.innerText = versionNum;
+}
+
 const homeIcon = document.querySelector('.pageIcon');
 const favicon = document.querySelector('link[rel="icon"]');
 
 updateLogo('logos/LogoDefault.png');
+
+updateVer('Version 2026.02.10.001');
